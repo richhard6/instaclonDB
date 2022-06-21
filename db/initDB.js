8f873463d48a009375916fbd4f6d83063d87a59d
Richard Perdomo <richard.perdomo121@gmail.com>
2022-06-21 09:12:08 +0200

    inden t

--- db/initDB.js
+++ db/initDB.js
@@ -16,53 +16,53 @@ async function main() {
         console.log('creating tables');
 
         await connection.query(`
-        CREATE TABLE users (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            username VARCHAR(100) NOT NULL,
-            password VARCHAR(100) NOT NULL,
-            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
-            )
-        `);
-
-        await connection.query(`
-        CREATE TABLE posts (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            userId INT NOT NULL,
-            FOREIGN KEY (userId) REFERENCES users(id),
-            picture VARCHAR(100) NOT NULL,
-            caption VARCHAR(200),
-            likes INT DEFAULT 0,
-            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            CREATE TABLE users (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                username VARCHAR(100) NOT NULL,
+                password VARCHAR(100) NOT NULL,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
                 )
             `);
 
         await connection.query(`
-        CREATE TABLE likes (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            postId INT NOT NULL,
-            FOREIGN KEY (postId) REFERENCES posts(id),
-            userId INT NOT NULL,
-            FOREIGN KEY (userId) REFERENCES users(id),
-            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
-        )
-        `);
+            CREATE TABLE posts (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                userId INT NOT NULL,
+                FOREIGN KEY (userId) REFERENCES users(id),
+                picture VARCHAR(100) NOT NULL,
+                caption VARCHAR(200),
+                likes INT DEFAULT 0,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+                    )
+                `);
 
         await connection.query(`
-        CREATE TABLE comments (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            postId INT NOT NULL,
-            FOREIGN KEY (postId) REFERENCES posts(id),
-            userId INT NOT NULL,
-            FOREIGN KEY (userId) REFERENCES users(id),
-            comment VARCHAR(200) NOT NULL,
-            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-            modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
-        )
-        `);
+            CREATE TABLE likes (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                postId INT NOT NULL,
+                FOREIGN KEY (postId) REFERENCES posts(id),
+                userId INT NOT NULL,
+                FOREIGN KEY (userId) REFERENCES users(id),
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            )
+            `);
+
+        await connection.query(`
+            CREATE TABLE comments (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                postId INT NOT NULL,
+                FOREIGN KEY (postId) REFERENCES posts(id),
+                userId INT NOT NULL,
+                FOREIGN KEY (userId) REFERENCES users(id),
+                comment VARCHAR(200) NOT NULL,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            )
+            `);
 
         console.log('tables created');
     } catch (err) {
