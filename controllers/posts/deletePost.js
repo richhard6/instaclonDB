b92068a6913039b2bc518ffc4ccdfd81d30cc09e
Richard Perdomo <richard.perdomo121@gmail.com>
2022-12-12 13:06:33 +0200

    Deleted functionability perfectly working

--- controllers/posts/deletePost.js
+++ controllers/posts/deletePost.js
@@ -4,20 +4,29 @@ const { generateError, deletePhoto } = require('../../helpers');
 
 const deletePost = async (req, res, next) => {
     try {
-        console.log('????');
         const { postId } = req.params;
 
         const post = await selectPostById(postId);
 
-        if (req.userId !== post.userId) {
+        const { user } = req;
+        //SE ESRTA BORRANDO KLA IMAGEN PERIO NO EL POST ª!! ! !" ASDFJKLAFSDHÑIOUADFSLNBJKZ"
+
+        if (user.id !== post.userId) {
             throw generateError('You are not the owner of this post ', 401);
         }
 
         if (post.picture) {
             await deletePhoto(post.picture);
         }
-    } catch (e) {
-        next(e);
+
+        await deletePostDb(post.id); // se borra pero se queda ahi pensativo
+
+        res.send({
+            status: 'ok',
+            message: 'post deleted',
+        });
+    } catch (err) {
+        next(err);
     }
 };
 
