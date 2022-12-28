9ec51486e1e82ec972c3710df8ca7a61e59ec668
Richard Perdomo <richard.perdomo121@gmail.com>
2022-12-28 20:25:57 +0100

    trying :)

--- db/getConnection.js
+++ db/getConnection.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2/promise');
 
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
 
+console.log(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
+
 let pool;
 
 const getConnection = async () => {
